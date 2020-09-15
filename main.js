c47713b27e135b8b41b298f62dd690d043d13af0
Tatsuya Fujii <61074883+tatsu081@users.noreply.github.com>
2020-09-16 06:18:13 +0900

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-function  hoverEffect() {
-    $(".item").mouseenter(function(){
-        $(this).addClass("hover")
-    }).mouseleave(function(){
-        $(this).removeClass("hover")
-    });
-} hoverEffect();
-
